e47cfd15c56e57f9c9b32022d1f229bf985dcbac
kinu-nu <ibnufaiz72tujuh@gmail.com>
2025-04-08 22:44:52 +0700

    Test CI/CD: update tampilan

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Azure App Service!');
+    res.send('✅ CI/CD berhasil! Ini versi terbaru dari app.');
 });
 
 app.listen(port, () => {
